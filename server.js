93e9ce41f2b9fd94050873e9cecbc588e0be8365
trees-arent-sleeping <transferdata76@gmail.com>
2023-05-21 12:12:23 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// connect to mongoDB
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongoDB connection error:"));
+db.once("open", () => {
+  console.log("connected to MongoDB");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
